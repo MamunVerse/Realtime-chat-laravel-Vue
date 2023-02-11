0497519fbb6a91dbe69d7c8cc62f0292fbac286b
Mamunur Rashid <49125977+MamunVerse@users.noreply.github.com>
2023-02-11 17:42:20 +0600

    Vuex store setup done

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,19 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+import storeVuex from './store/index';
+
+const store = new Vuex.Store(storeVuex);
+
+
+
 Vue.component('main-app', require('./components/MainApp.vue').default);
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    store
 });
